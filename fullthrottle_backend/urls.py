e853392e238a05e3536338cd827d16c398981482
war_machine <niranjantiwari667@gmail.com>
2020-09-27 03:29:21 +0530

    url issue solved

--- fullthrottle_backend/urls.py
+++ fullthrottle_backend/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 
+from django.contrib import admin
+from django.urls import path
+from activity_api.views import UserActivityView
 urlpatterns = [
+    path('', UserActivityView.as_view(),name='UserActiivtyView'),
     path('admin/', admin.site.urls),
-    path('', include('activity_api.urls')),
 ]
