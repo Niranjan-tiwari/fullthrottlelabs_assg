f68595c084a6066e71c12e91b9569637c9203344
Niranjan Tiwari <40960634+Niranjan-tiwari@users.noreply.github.com>
2020-09-27 04:18:52 +0530

    Update urls.py

--- activity_api/urls.py
+++ activity_api/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import url
 from .views import UserActivityView
 
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
 url(r'user/activity_details/$',UserActivityView.as_view(),name='user_activity'),
-
 ]
